c4f5b9a33df1ed154234e469263ecf0da35fa471
Anastasia <averkon8@gmail.com>
2020-02-18 16:16:30 +0200

    ObjectPage tests for main page

--- conftest.py
+++ conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from selenium import webdriver
+import time
+from selenium.webdriver.chrome.options import Options
 
 
 # забираем параметр языка
@@ -8,14 +10,16 @@ def pytest_addoption(parser):
                      help="Choose language: en, es, fr...")
 
 
-# открываем окно с заданным языком
 @pytest.fixture(scope="function")
 def browser(request):
-    language_name = request.config.getoption("language")
-    link = f"http://selenium1py.pythonanywhere.com/{language_name}/catalogue/coders-at-work_207/"
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    browser.get(link)
+    # Передаём язык из cmd
+    user_language = request.config.getoption('language')
+    # Инициализируются опции браузера
+    options = Options()
+    # В опции вебдрайвера передаем параметр из командной строки
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
+    time.sleep(10)
     browser.quit()
