9707510c65b67e4c9d825020b79f7d15ed672241
Anastasia <averkon8@gmail.com>
2020-02-24 20:57:06 +0200

    test for product page

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,11 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTR_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    ADD_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div > h1")
+    PRODUCT_IN_CART = (By.CSS_SELECTOR, "#messages :nth-child(1) .alertinner :nth-child(1) ")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main> .price_color")
+    PRICE_IN_CART = (By.CSS_SELECTOR, ".alert-info  > .alertinner > p > strong")
---
+++ pages/product_page.py
@@ -0,0 +1,35 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+from selenium.common.exceptions import NoAlertPresentException
+import  math
+
+class ProductPage(BasePage):
+    def add_to_the_cart(self):
+        button = self.browser.find_element(*ProductPageLocators.ADD_BUTTON)
+        button.click()
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
+
+
+    def should_be_right_product_name(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        name_in_cart = self.browser.find_element(*ProductPageLocators.PRODUCT_IN_CART).text
+        assert product_name == name_in_cart, "Wrong product name in cart"
+
+
+    def should_be_right_price(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        price_in_cart = self.browser.find_element(*ProductPageLocators.PRICE_IN_CART).text
+        assert product_price == price_in_cart, "Wrong price in cart"
